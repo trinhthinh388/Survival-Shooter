98d85435b8fc37d009dfe474c9687580076baa4a
Trinh Duc Thinh <trinhthinh388@gmail.com>
2018-05-16 10:37:13 +0700

    Change

---
+++ Survival Shooter/Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public Transform target;
+	public float smoothSpeed;
+	Vector3 offset;
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - target.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		Vector3 targetCam = target.position + offset;
+		transform.position = Vector3.Lerp(transform.position, targetCam, smoothSpeed * Time.deltaTime);
+	}
+}
