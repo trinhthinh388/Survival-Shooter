63270239f797e16787ed96d6580cccef5c5dbd8d
Trinh Duc Thinh <trinhthinh388@gmail.com>
2018-05-23 11:01:20 +0700

    Change

---
+++ New Unity Project/Assets/Scripts/Managers/EnemyManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CompleteProject
+{
+    public class EnemyManager : MonoBehaviour
+    {
+        public static int EnemyAlive = 0;
+        public Wave[] waves;
+        public Transform spawnPoint;
+        public float TimeBetweenWaves = 5f;
+        private float countdown = 2f;
+        private int waveIndex = 0;
+
+        void Update()
+        {
+            if (EnemyAlive > 0)
+                return;
+            if(countdown <= 0)
+            {
+                StartCoroutine(SpawnWave());
+                countdown = TimeBetweenWaves;
+                return;
+            }
+            countdown -= Time.deltaTime;
+
+        }
+        IEnumerator SpawnWave()
+        {
+            Wave wave = waves[waveIndex];
+            EnemyAlive = wave.count[waveIndex];
+            for (int i = 0; i < wave.count[waveIndex]; i++)
+            {
+                for (int j = 0; j < wave.enemy.Length; j++)
+                {
+                    SpawnEnemy(wave.enemy[j]);
+                    yield return new WaitForSeconds(1f / wave.rate);
+                }
+            }
+
+            waveIndex++;
+        }
+
+        void SpawnEnemy(GameObject enemy)
+        {
+            Instantiate(enemy, spawnPoint.position, spawnPoint.rotation);
+        }
+    }
+}
